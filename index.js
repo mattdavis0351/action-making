5faee3cf46de79bcda7016823914a0601163e961
mattdavis0351 <mattdavis0351@github.com>
2020-02-16 14:31:17 -0800

    move again

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
-// const request = require("request-promise");
 const core = require("@actions/core");
 const github = require("@actions/github");
-const request = require("request-promise");
-const { WebClient } = require("@slack/web-api");
-
-// const web = new WebClient(process.env.SLACK_TOKEN);
-// const web = new WebClient(process.env.SLACK_BOT_TOKEN);
-
-// The current date
-// const currentTime = new Date().toTimeString();
+// const { Octokit } = require("@octokit/rest")
 async function run() {
   try {
-    // const GITHUB_TOKEN = core.getInput("repo-token");
-    const GITHUB_TOKEN = process.env.GITHUBTOKEN;
-    const token = process.env.REPOTOKEN;
-    // const SLACK_BOT_TOKEN = core.getInput("slackAuth");
-    // const SLACK_CHANNEL = core.getInput("channel");
-    const octokit = new github.GitHub(GITHUB_TOKEN);
-
-    // const options = {
-    //   //   channel: SLACK_CHANNEL,
-    //   //   text: ""
-    //   method: "GET",
-    //   uri: `https://github.com/${process.env.GITHUB_ACTOR}/repos/${process.env.GITHUB_ACTOR}/${process.env.GITHUB_REPO}/pulls`,
-    //   auth: `token ${token}`
-    // };
-
-    const reviewers = await octokit.issues.listForRepo({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo
+    // const issueTitle = core.getInput("issue-title");
+    // const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
+    const octokit = new github.GitHub(token);
+    const reviewers = await octokit.pulls.list({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner
     });
-    // const reviewers
+
     console.log(reviewers);
-  } catch (err) {
-    // core.debug(err);
+  } catch (error) {
+    core.setFailed(error.message);
   }
 }
-
 run();
-
 // (async () => {
 //   try {
 //     // Use the `chat.postMessage` method to send a message from this app
