031340221c89d31bd1afc50b14caa0d748324c6c
mattdavis0351 <mattdavis0351@github.com>
2020-02-16 14:24:02 -0800

    remove git input

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const request = require("request-promise");
-// const core = require("@actions/core");
-// const github = require("@actions/github");
+const core = require("@actions/core");
+const github = require("@actions/github");
 const request = require("request-promise");
 const { WebClient } = require("@slack/web-api");
 
@@ -11,28 +11,29 @@ const { WebClient } = require("@slack/web-api");
 // const currentTime = new Date().toTimeString();
 async function run() {
   try {
-    // const GITHUB_TOKEN = core.getInput("repoToken");
+    const GITHUB_TOKEN = core.getInput("repo-token");
+    // const GITHUB_TOKEN = process.env.GITHUBTOKEN
     const token = process.env.REPOTOKEN;
     // const SLACK_BOT_TOKEN = core.getInput("slackAuth");
     // const SLACK_CHANNEL = core.getInput("channel");
-    // const octokit = new github.GitHub(GITHUB_TOKEN);
+    const octokit = new github.GitHub(GITHUB_TOKEN);
 
-    const options = {
-      //   channel: SLACK_CHANNEL,
-      //   text: ""
-      method: "GET",
-      uri: `https://github.com/${process.env.GITHUB_ACTOR}/repos/${process.env.GITHUB_ACTOR}/${process.env.GITHUB_REPO}/pulls`,
-      auth: `token ${token}`
-    };
+    // const options = {
+    //   //   channel: SLACK_CHANNEL,
+    //   //   text: ""
+    //   method: "GET",
+    //   uri: `https://github.com/${process.env.GITHUB_ACTOR}/repos/${process.env.GITHUB_ACTOR}/${process.env.GITHUB_REPO}/pulls`,
+    //   auth: `token ${token}`
+    // };
 
-    // const reviewers = await octokit.issues.listForRepo({
-    //   owner: github.context.repo.owner,
-    //   repo: github.context.repo.repo
-    // });
-    const reviewers = await request(options);
+    const reviewers = await octokit.issues.listForRepo({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    });
+    const reviewers
     console.log(reviewers);
   } catch (err) {
-    // core.debug(err);
+    core.debug(err);
   }
 }
 
